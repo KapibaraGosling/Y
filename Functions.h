9b460ac1958e27e9f740fce65c565735fda509ac
KapibaraGosling <137946228+KapibaraGosling@users.noreply.github.com>
2025-03-02 21:24:14 +0300

    Пока без библии

---
+++ Functions.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Polygon.h"
+#include "Point.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cmath>
+using std::cout;
+using std::cin;
+/*
+@brief Функция запрашивает ввод числа с клавиатуры и проверяет его корректность.
+@return возвращает введенное пользователем число типа int.
+*/
+int inputInt(void);
+
+/*
+@brief Функция проверки того, лежат ли 3 точки на одной прямой.
+@param p1 - первая точка
+@param p2 - вторая точка
+@param p3 - третья точка
+@return возвращает результат проверки.
+*/
+bool OnSameLine(const Point& p1, const Point& p2, const Point& p3);
---
+++ Point.cpp
@@ -0,0 +1,46 @@
+﻿#include "Point.h"
+bool operator==(const Point& point1, const Point& point2)
+{
+    return point1.getX() == point2.getX() && point1.getY() == point2.getY();
+}
+
+bool operator!=(const Point& point1, const Point& point2)
+{
+    return !(point1 == point2);
+}
+
+
+Point Point::operator+(const Point& other) const {
+    return Point(this->x + other.x, this->y + other.y);
+}
+Point Point::operator-(const Point& other) const {
+    return Point(this->x - other.x, this->y - other.y);
+}
+
+std::istream& operator>>(std::istream &cinP, Point& point) {
+    cinP >> point.x >> point.y;
+    if (cinP.fail())
+    {
+        throw std::invalid_argument("Incorrect value entered");
+    }
+    return cinP;
+}
+
+std::ostream& operator<<(std::ostream &coutP, Point& point) {
+    coutP << "(" << point.x << "; " << point.y << ")\n";
+    return coutP;
+}
+
+
+Point::Point(const int x, const int y) :
+    x{ x }, y{ y }{}
+
+int Point::getX() const
+{
+    return x;
+}
+
+int Point::getY() const
+{
+    return y;
+}
